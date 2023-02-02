53c659ef207e3b011722b60073aaed1bb84715c3
uzunhair <uzunhair@yandex.ru>
2023-02-02 13:57:10 +0300

    Optimise header code

--- src/components/Shared/HomeFourFiveNavBar/HomeFourFiveNavBar.js
+++ src/components/Shared/HomeFourFiveNavBar/HomeFourFiveNavBar.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 import {
   UilTelegram,
   UilWhatsappAlt,
@@ -8,9 +9,25 @@ import {
   UilPhone,
   UilClock,
   UilPhoneVolume,
+  UilBars,
 } from '@iconscout/react-unicons';
 import { StaticImage } from 'gatsby-plugin-image';
 // import Sidebar from '../Sidebar/Sidebar';
+
+import logoImg from '../../../images/logo/logo.svg';
+
+const NavBtn = styled.button`
+  background-color: transparent;
+  border-width: 0;
+`;
+
+const Logo = styled(Link)`
+  max-width: 160px;
+
+  @media (min-width: 768px) {
+    max-width: 230px;
+  }
+`;
 
 const HomeFourFiveNavBar = () => {
   const [show, setShow] = useState(false);
@@ -33,22 +50,22 @@ const HomeFourFiveNavBar = () => {
               <div className="col-lg">
                 <div className="header-info header-info4 p-0">
                   <span>
-                    <Link to="mailto:amirkhanov.clinique@gmail.com">
+                    <a href="mailto:amirkhanov.clinique@gmail.com">
                       <UilEnvelope size={18} color="#e12454" style={{ marginRight: 8 }} />
                       amirkhanov.clinique@gmail.com
-                    </Link>
+                    </a>
                   </span>
                   <span>
-                    <Link to="tel:+79654887700">
+                    <a href="tel:+79654887700">
                       <UilPhone size={18} color="#e12454" style={{ marginRight: 8 }} />
                       +7‒965‒488‒77‒00
-                    </Link>
+                    </a>
                   </span>
                   <span>
-                    <Link to="https://yandex.ru/maps/?from=api-maps&ll=47.516468%2C42.984490&mode=routes&origin=jsapi21&rtext=~42.972454%2C47.490812&rtt=auto&ruri=~ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgozNjk1MjAyNzI0EnLQoNC%2B0YHRgdC40Y8sINCg0LXRgdC%2F0YPQsdC70LjQutCwINCU0LDQs9C10YHRgtCw0L0sINCc0LDRhdCw0YfQutCw0LvQsCwg0YPQu9C40YbQsCDQptC40L7Qu9C60L7QstGB0LrQvtCz0L4sIDEy0JIiCg2X9j1CFczjK0I%3D&z=13.16">
+                    <a href="https://yandex.ru/maps/?from=api-maps&ll=47.516468%2C42.984490&mode=routes&origin=jsapi21&rtext=~42.972454%2C47.490812&rtt=auto&ruri=~ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgozNjk1MjAyNzI0EnLQoNC%2B0YHRgdC40Y8sINCg0LXRgdC%2F0YPQsdC70LjQutCwINCU0LDQs9C10YHRgtCw0L0sINCc0LDRhdCw0YfQutCw0LvQsCwg0YPQu9C40YbQsCDQptC40L7Qu9C60L7QstGB0LrQvtCz0L4sIDEy0JIiCg2X9j1CFczjK0I%3D&z=13.16">
                       <UilClock size={18} color="#e12454" style={{ marginRight: 8 }} />
                       г. Махачкала, ул. Циолковского 12в
-                    </Link>
+                    </a>
                   </span>
                 </div>
               </div>
@@ -57,32 +74,32 @@ const HomeFourFiveNavBar = () => {
                   <div className="top4-menu">
                     <ul className="list-inline">
                       <li>
-                        <a className="need-help" href="#">
+                        <a className="need-help" href="#1">
                           Нужна помощь?
                         </a>
                       </li>
                       <li>
-                        <a href="#">Прайс лист</a>
+                        <a href="#1">Прайс лист</a>
                       </li>
                       <li>
-                        <a href="#">Медиа</a>
+                        <a href="#1">Медиа</a>
                       </li>
                     </ul>
                   </div>
                   <div className="header-social-icons top4-social d-none d-xl-block">
                     <ul>
                       <li>
-                        <a href="#">
+                        <a href="#1">
                           <UilTelegram size={24} color="#b2bfcf" />
                         </a>
                       </li>
                       <li>
-                        <a href="#">
+                        <a href="#1">
                           <UilWhatsappAlt size={24} color="#b2bfcf" />
                         </a>
                       </li>
                       <li>
-                        <a href="#">
+                        <a href="#1">
                           <UilInstagramAlt size={24} color="#b2bfcf" />
                         </a>
                       </li>
@@ -102,12 +119,10 @@ const HomeFourFiveNavBar = () => {
           }>
           <div className="container-fluid">
             <div className="row">
-              <div className="col-xl-7 col-lg-12 col-md-12 d-flex align-items-center navbar__wrapper">
-                <div className="logo pos-rel">
-                  <Link to="/">
-                    <StaticImage src="../../../images/logo/logo.svg" alt="A dinosaur" quality={100} />
-                  </Link>
-                </div>
+              <div className="col-xl col-lg-12 col-md-12 d-flex align-items-center navbar__wrapper">
+                <Logo to="/">
+                  <img src={logoImg} alt="Клиника Амирханова" />
+                </Logo>
                 <div className="header__menu header__menu4 pl-60">
                   <nav id="mobile-menu">
                     <ul>
@@ -127,44 +142,41 @@ const HomeFourFiveNavBar = () => {
                   </nav>
                 </div>
 
-                <div onClick={handleShow} className="side-menu-icon d-lg-none text-end">
-                  <button className="side-toggle border-0 bg-transparent">
-                    <i className="fas fa-bars" />{' '}
-                  </button>
-                </div>
+                <NavBtn className="side-toggle d-lg-none" onClick={handleShow}>
+                  <UilBars size={28} color="#10111e" />
+                </NavBtn>
               </div>
-              <div className="col-xl-5 col-lg-9 col-md-9 d-none d-xl-flex d-flex align-items-center justify-content-end">
+              <div className="col-xl-auto col-lg-9 col-md-9 d-none d-xl-flex d-flex align-items-center justify-content-end">
                 <div className="d-flex align-items-center justify-content-end">
-                  <div className="header-lang pos-rel d-none d-lg-block">
+                  <div className="header-lang pos-rel d-none d-xxl-block">
                     <div className="lang-icon">
                       <UilPhoneVolume />
-                      <span>
-                        Администраторы
-                      </span>
+                      <span>Администраторы</span>
                     </div>
                     <ul className="header-lang-list">
                       <li>
-                        <a href="#">USA</a>
+                        <a href="#1">USA</a>
                       </li>
                       <li>
-                        <a href="#">UK</a>
+                        <a href="#1">UK</a>
                       </li>
                       <li>
-                        <a href="#">CA</a>
+                        <a href="#1">CA</a>
                       </li>
                       <li>
-                        <a href="#">AU</a>
+                        <a href="#1">AU</a>
                       </li>
                     </ul>
                   </div>
                   <div className="header-button pl-50">
                     <Link
                       to="/contact"
-                      className="primary_btn btn-icon ml-0"
+                      className="primary_btn btn-icon ml-0 d-flex"
                       style={{ animationDelay: '0.6s' }}
                       tabIndex="0">
                       <span>+</span>
-                      Позвонить в клинику
+                      <div>Позвонить</div>
+                      <div className="d-none d-xl-block pl-5"> в клинику</div>
                     </Link>
                   </div>
                 </div>
