b284cf7305abf9788f0fcb1bc00e623de3091d7c
uzunhair <uzunhair@yandex.ru>
2023-02-22 17:18:17 +0300

    Add page services

---
+++ src/pages/services.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from 'components/layout/layout';
+
+const ServicesPage = () => {
+  return (
+    <Layout>
+      <div>Hello</div>
+    </Layout>
+  );
+};
+
+export default ServicesPage;
