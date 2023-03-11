a053c96547a4febc948a03529046387c8c453fe2
uzunhair <uzunhair@yandex.ru>
2023-03-11 04:32:49 +0300

    Delete noscript

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -24,10 +24,5 @@ exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents, setPostBodyCompo
         });`,
       }}
     />,
-    <noscript key="yandex-noscript">
-      <div>
-        <img src="https://mc.yandex.ru/watch/66513775" style={{ position: 'absolute', left: '-9999px' }} alt="" />
-      </div>
-    </noscript>,
   ]);
 };
